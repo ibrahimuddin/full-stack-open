7a05f241e240df0ac2fee596d0f200dca63f3a2e
ibrahimuddin <ibrahimu0203@gmail.com>
2024-06-05 16:04:50 +0100

    complete courseinfo step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,39 +1,4 @@
-const Header = ({course}) => {
-  return <h1>{course}</h1>
-}
-
-const Total = ({parts}) => {
-  const total = parts.reduce((acc,part) =>{
-    return acc +=part.exercises
-  } ,0)
-  return <p>Number of exercises {total}</p>
-}
-
-const Part = ({part, exercises}) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from "./components/Course" 
 
 
 
