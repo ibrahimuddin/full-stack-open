cbbe9881c67100c782b7411c76e4c1e2d0447a98
ibrahimuddin <ibrahimu0203@gmail.com>
2025-05-07 13:14:59 +0100

    4.4 complete

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -7,3 +7,39 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result,1)
 })
+
+describe('total likes', () => {
+    test("empty list total likes is 0", () => {
+        assert.strictEqual(listHelper.totalLikes([]),0)
+    })
+    test("like total correct when only one blog", () => {
+        const blogPost = {
+            title: "test",
+            author: "ibrahim",
+            url: "testing123",
+            likes: 10
+        } 
+        assert.strictEqual(listHelper.totalLikes([blogPost]),10)
+    })
+    test("multiple blogs", () => {
+        const blogPost1 = {
+            title: "test",
+            author: "ibrahim",
+            url: "testing123",
+            likes: 10
+        }
+        const blogPost2 = {
+            title: "test2",
+            author: "ibrahim",
+            url: "testing123",
+            likes: 12
+        }
+        const blogPost3 = {
+            title: "test3",
+            author: "ibrahim",
+            url: "testing123",
+            likes: 13
+        }
+        assert.strictEqual(listHelper.totalLikes([blogPost1,blogPost2,blogPost3]),35)
+    })
+})
