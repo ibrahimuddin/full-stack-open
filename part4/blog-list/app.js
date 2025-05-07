ed23a2947bbb02f91f54493d8ee9e08ac1024cd5
ibrahimuddin <ibrahimu0203@gmail.com>
2025-05-07 12:31:06 +0100

    4.2 complete

---
+++ part4/blog-list/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const { PORT, MONGODB_URI } = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(MONGODB_URI)
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
